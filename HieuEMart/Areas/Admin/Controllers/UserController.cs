028a5ed1f0fd8442f4602a580548eaf9f6ed4250
BHieeuss <tbh.11203@gmail.com>
2024-11-02 21:19:24 +0700

    add create user via admin panel

--- HieuEMart/Areas/Admin/Controllers/UserController.cs
+++ HieuEMart/Areas/Admin/Controllers/UserController.cs
@@ -1,6 +1,10 @@
-﻿using HieuEMart.Repository;
+﻿using HieuEMart.Models;
+using HieuEMart.Repository;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Numerics;
 
 namespace HieuEMart.Areas.Admin.Controllers
 {
@@ -8,16 +12,96 @@ namespace HieuEMart.Areas.Admin.Controllers
     [Route("Admin/User")]
     public class UserController : Controller
     {
-        private readonly DataContext _dataContext;
-        public UserController(DataContext context)
+        private readonly UserManager<AppUserModel> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserController(UserManager<AppUserModel> userManager, RoleManager<IdentityRole> roleManager)
         {
-            _dataContext = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+        }
+        [HttpGet]
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _userManager.Users.OrderByDescending(p => p.Id).ToListAsync());
         }
 
-        [Route("Index")]
-        public async Task <IActionResult> Index()
+        [HttpGet]
+        [Route("Create")]
+        public async Task<IActionResult> Create()
         {
-            return View(await _dataContext.Users.OrderByDescending(p => p.Id).ToListAsync());
+            var roles = await _roleManager.Roles.ToListAsync();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+            return View(new AppUserModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("Create")]
+        public async Task<IActionResult> Create(AppUserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var createUserResult = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (createUserResult.Succeeded)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    AddIdentityErrors(createUserResult);
+                    return View(user);
+                }
+            }
+            else
+            {
+                TempData["error"] = "Vui lòng kiểm tra lại dữ liệu!";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            var roles = await _roleManager.Roles.ToListAsync();
+            ViewBag.Roles = new SelectList(roles, "Id", "Name");
+            return View(user);
+        }
+
+        [HttpGet]
+        [Route("Delete")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if(string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var deleteResult = await _userManager.DeleteAsync(user);
+            if (!deleteResult.Succeeded)
+            {
+                return View("Error");
+            }
+            TempData["Success"] = "Xóa Người dùng thành công!";
+            return RedirectToAction("Index");
+        }
+
+        private void AddIdentityErrors(IdentityResult identityResult)
+        {
+            foreach(var error in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
