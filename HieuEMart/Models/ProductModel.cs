4c0cfe20f96970d33294d62fd686a9886e4ad5b6
BHieeuss <tbh.11203@gmail.com>
2024-12-16 21:12:59 +0700

    .

--- HieuEMart/Models/ProductModel.cs
+++ HieuEMart/Models/ProductModel.cs
@@ -16,7 +16,7 @@ namespace HieuEMart.Models
         public string Description { get; set; }
 		[Required(ErrorMessage = "Yêu cầu nhập giá Sản phẩm")]
 		[Range(0.01, double.MaxValue)]
-		[Column(TypeName = "decimal(8, 2)")]
+		[Column(TypeName = "decimal(18, 2)")]
 		public decimal Price { get; set; }
 		[Required, Range(1, int.MaxValue, ErrorMessage = "Chọn một thương hiệu")]
 		public int BrandId { get; set; }
